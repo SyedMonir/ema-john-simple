78618ad28375311efe830e49558025b7b47f1bd6
Syed Monirul Alam <Rambling.ctg@gmail.com>
2022-03-22 10:55:25 +0600

    card ready

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import { Button, Card } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
 import './Product.css';
 
 const Product = (props) => {
-    const { category, img, name, price, rating, seller } = props.product;
-    return (
-        <div>
-            <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src={img} />
-                <Card.Body>
-                    <Card.Title>{name}</Card.Title>
-                    <Card.Text>
-                        $ {price}
-                    </Card.Text>
-                    <Card.Footer>
-                        <Button variant="primary">Go somewhere</Button>
-                    </Card.Footer>
-
-                </Card.Body>
-            </Card>
-        </div>
-    );
+  const { img, name, price, ratings, ratingsCount, seller } =
+    props.product;
+  console.log(name);
+  return (
+    <div className="product">
+      <Card>
+        <Card.Img variant="top" src={img} />
+        <h6>{name}</h6>
+        <p className="product-price">Price: ${price}</p>
+        <p className="cardFooter">Manufacturer : {seller}</p>
+        <p className="rating">
+          Rating: {ratings} ({ratingsCount} Users)
+        </p>
+      </Card>
+      <button className="addToCart">Add To Cart</button>
+    </div>
+  );
 };
 
 export default Product;
