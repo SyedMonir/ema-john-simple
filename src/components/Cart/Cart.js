cdcfeb63a6428e28e3cedbec5c9f4fe5ff09fcef
Syed Monirul Alam <Rambling.ctg@gmail.com>
2022-03-22 23:23:54 +0600

    added cart calculation

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,14 +2,26 @@ import React from 'react';
 import './Cart.css';
 
 const Cart = ({cart}) => {
+
+  let total = 0;
+  let shipping = 0
+  for (const product of cart) {
+    total = total + product.price;
+    shipping = shipping + product.shipping;
+  };
+
+  const tax = (total * 0.1).toFixed(2);
+
+  const grandTotal = total + shipping + parseFloat(tax);
+
   return (
     <div>
       <h5>Order Summary</h5>
       <p>Selected Items: {cart.length}</p>
-      <p>Total Price: ${}</p>
-      <p>Total Shipping Charge: ${}</p>
-      <p>Total Tax: ${}</p>
-      <h6>Grand Total: ${}</h6>
+      <p>Total Price: ${total}</p>
+      <p>Total Shipping: ${shipping}</p>
+      <p>Total Tax: ${tax}</p>
+      <h6>Grand Total: ${grandTotal}</h6>
     </div>
   );
 };
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -6,11 +6,15 @@ import './Product.css';
 
 const Product = ({ product, handleAddToCart }) => {
   const { img, name, price, ratings, ratingsCount, seller } = product;
+  // if(name.length > 20){
+  //   const newName = name.slice(0,20);
+  //   console.log(newName);
+  // }
   return (
     <div className="product">
       <Card>
         <Card.Img variant="top" src={img} />
-        <h6>{name}</h6>
+        <h6> {name}</h6>
         <p className="product-price">Price: ${price}</p>
         <p className="cardFooter">Manufacturer : {seller}</p>
         <p className="rating">
