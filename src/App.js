80cd2909cf1d4e8253a39edc9ad42c8083e0e8c0
Syed Monirul Alam <Rambling.ctg@gmail.com>
2022-03-31 09:16:28 +0600

    added routes and some compo

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header/Header';
 import Shop from './components/Shop/Shop';
+import { Route, Routes } from 'react-router-dom';
+import Orders from './components/Orders/Orders';
+import Inventory from './components/Inventory/Inventory';
+import About from './components/About/About';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Shop />
+      <Routes>
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/inventory" element={<Inventory />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,32 +2,33 @@ import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import './Header.css';
 import logo from '../../images/logo.svg';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-    return (
-        <nav id='navbar'>
-            <Navbar expand="lg">
-                <Container>
-                    <Navbar.Brand href="#home">
-                        <img
-                            src={logo}
-                            className="d-inline-block align-top"
-                            alt="Ema-John"
-                        />
-                    </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="ms-auto">
-                            <Nav.Link href="/shop">Shop</Nav.Link>
-                            <Nav.Link href="/order">Order</Nav.Link>
-                            <Nav.Link href="/inventory">Inventory</Nav.Link>
-                            <Nav.Link href="/about">About</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </nav>
-    );
+  return (
+    <nav id="navbar">
+      <Navbar expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              src={logo}
+              className="d-inline-block align-top"
+              alt="Ema-John"
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Link to={'/shop'}>Shop</Link>
+              <Link to={'/orders'}>Order</Link>
+              <Link to={'/inventory'}>Inventory</Link>
+              <Link to={'/about'}>About</Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </nav>
+  );
 };
 
 export default Header;
